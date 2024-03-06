6ada78e8344b7ffd9e88d2b9d9e0677dc1afa32b
Angel <107006578+sleeyderx@users.noreply.github.com>
2024-03-05 20:48:46 -0600

    hello with gradle

---
+++ gradle-tutorial/src/main/java/com/loaizang/GradleTutorial.java
@@ -0,0 +1,8 @@
+package com.loaizang;
+
+public class GradleTutorial{
+
+	public static void main(String []args){
+		System.out.println("Gradle 4tw!");
+	}
+}
---
+++ gradle-tutorial/src/test/java/com/loaizang/GradleTutorialTest.java
@@ -0,0 +1,11 @@
+package com.loaizang;
+
+import org.junit.Test;
+
+public class GradleTutorialTest {
+
+	@Test
+	public void verifyNoExceptionThrown(){
+		GradleTutorial.main(new String[]{});
+	}
+}
